0361b7c026478c02746801a473cf792957c38705
qa-exercises <118732445+qa-exercises@users.noreply.github.com>
2022-12-22 01:11:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-example-array-destructuring
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-example-array-destructuring)
---
+++ index.js
@@ -0,0 +1,11 @@
+// 🔎 Example: Array destructuring
+
+const fruits = ['🍎', '🥝', '🍇']
+
+// Without destructuring:
+const apple_ = fruits[0]
+const kiwi_ = fruits[1]
+const grapes_ = fruits[2]
+
+// With destructuring (equivalent to the code above):
+const [apple, kiwi, grapes] = fruits
